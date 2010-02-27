9a3aaa3852edd46b509675c8b581cc664a924411
mewlist <mewlist@mewlist.com>
2010-02-27 13:38:32 +0900

    RDoc document

--- lib/rask.rb
+++ lib/rask.rb
@@ -7,37 +7,66 @@ require 'thread'
 
 require File.dirname(__FILE__) + '/rask/state_machine'
 
-
+#Authors::   mewlist / Hidenori Doi
+#Copyright:: Copyright (C) 2010 mewlist / Hidenori Doi
+#License::   The MIT License
+#
+#== Rask is terminatable task engine
+#
 module Rask
   
+  #
+  #===Task base class
+  #To define new Task you must inherit this base-class
+  #* sample code
+  # class NewTask < Rask::Task
+  #   define_state :initial, :initial => true
+  #   define_state :finish
+  #   def initial
+  #     transition :finish
+  #   end
+  #   def finish
+  #     destroy
+  #   end
+  # end
   class Task
     include StateMachine
     attr_accessor :task_id
     attr_accessor :group
     
+    #
+    #====If group option is given, the task is classified by group name.
+    #
+    #==== _group
+    # group name to classify
     def initialize(_group=nil)
       group = _group
       super()
     end
     
-    
+    #
+    #====automatically callbacked from task engine.
+    #
     def run
       if @state
         eval @state.to_s
       end
     end
     
-    
+    #
+    #====Transition to new state. In the state function.
+    # Usually you should call generated transition_to_[state name] function
     def transition(to)
       @state = to
     end
     
     
+    #
     def destroy
       transition nil
     end
     
-    
+    #
     def destroy?
       @state == nil
     end
@@ -46,42 +75,41 @@ module Rask
   
   
   @@base_dir         = '/tmp/rask'
-  @@threading        = false
   @@thread_max_count = 5
   @@thread_count     = 0
   @@terminated       = false
   @@queue            = Queue.new
   @@processing       = []
   @@locker           = Mutex::new
- 
+  
+  #
+  #====Set base storage directory
+  #default is '/tmp/rask'
+  #
   def self.base_directory=(new_directory)
     @@base_dir = new_directory
   end
   
-  def self.enable_thread
-    @@threading = true
-  end
-  
-  def self.disable_thread
-    @@threading = false
-  end
-  
+  #
+  #====Set max count of worker thread
+  #
   def self.thread_max_count=(count)
     @@thread_max_count = count
   end
   
+  #
   def self.task_path(task_id)
     @@base_dir+"/#{task_id}.task"
   end
   
-  def self.task_path(task_id)
-    @@base_dir+"/#{task_id}.task"
-  end
-  
+  #
   def self.pid_path
     @@base_dir+"/#{File.basename($0)}.pid"
   end
   
+  #
+  #====Insert new task 
+  # Rask::insert NewTask.new
   def self.insert(task)
     initialize_storage
     task_id = "#{safe_class_name(task.class.name)}-#{task.group.to_s}-#{Time.now.to_i}-#{Time.now.usec}"
@@ -95,6 +123,7 @@ module Rask
   end
   
   
+  #
   def self.run(task_path)
     f = File.open(task_path, 'r+') rescue return
     f.flock(File::LOCK_EX)
@@ -110,18 +139,8 @@ module Rask
     FileUtils.rm(task_path) if task.destroy?
   end
   
-  def self.each(options = { :class=>nil, :group=>nil }, &blk)
-    threads = []
-    tasks(options).each { |d|
-      if @@threading
-        threads << Thread::new(d) { |task_path| run(task_path, &blk) }
-      else
-        run(d, &blk)
-      end
-    }
-    threads.each { |t| t.join } if @@threading
-  end
-  
+  #====Get all file path of task list
+  #
   def self.tasks(options = { :class=>nil, :group=>nil })
     target = @@base_dir
     target += '/' if options[:class] || options[:group]
@@ -137,20 +156,30 @@ module Rask
     task_list
   end
   
+  #
   def self.initialize_storage
     unless File.exists? @@base_dir
       FileUtils.makedirs @@base_dir
     end
   end
   
+  #
+  #====force destroy the task
+  #
   def self.destroy(task)
     FileUtils.rm(task_path(task.task_id)) if File.exists? task_path(task.task_id)
   end
   
+  #
   def self.safe_class_name(c)
     c.gsub(/[:]/,'@')
   end
-
+  
+  #
+  #====Start daemon process
+  #It is possible to specify the the task group.
+  # Rask.daemon(:group=>'TaskGroup')
+  #
   def self.daemon(options = { :class=>nil, :group=>nil, :sleep=>0.1 })
     print "daemon start\n"
     exit if fork
@@ -203,6 +232,7 @@ module Rask
     end
   end
   
+  #
   def self.safe_exit
     print "daemon terminated"
     @@terminated = true
